231b1075e408ea62092495dd2b4a6d53bf77eddc
Kah Wai <kahwai@healthmetrics.co>
2021-07-29 15:29:33 +0800

    Add pathParameters

--- lib/src/bindings/bindable_base.dart
+++ lib/src/bindings/bindable_base.dart
@@ -50,23 +50,28 @@ class InitParam {
   final dynamic param;
   final String? hashAnchor;
   final Map<String, List<String>>? deepLinkRouteParam;
+  final Map<String, String>? pathParameters;
 
   const InitParam({
     this.param,
     this.hashAnchor,
     this.deepLinkRouteParam,
+    this.pathParameters,
   });
 
   InitParam copyWith({
     dynamic param,
     String? hashAnchor,
     Map<String, List<String>>? deepLinkRouteParam,
-  }) =>
-      InitParam(
-        param: param ?? this.param,
-        deepLinkRouteParam: deepLinkRouteParam ?? this.deepLinkRouteParam,
-        hashAnchor: hashAnchor ?? this.hashAnchor,
-      );
+    Map<String, String>? pathParameters,
+  }) {
+    return InitParam(
+      param: param ?? this.param,
+      hashAnchor: hashAnchor ?? this.hashAnchor,
+      deepLinkRouteParam: deepLinkRouteParam ?? this.deepLinkRouteParam,
+      pathParameters: pathParameters ?? this.pathParameters,
+    );
+  }
 }
 
 bool checkIsMap(dynamic param) {
